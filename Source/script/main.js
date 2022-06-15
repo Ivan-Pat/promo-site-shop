aae31a6efa88482b156c4f045a298f55f11567c9
Patrick <patrin.ii@bk.ru>
2022-06-15 16:00:13 +0300

    Add swiper fruit

--- Source/script/main.js
+++ Source/script/main.js
@@ -31,8 +31,51 @@ import Swiper, { Navigation, Pagination } from '../../Gulp/node_modules/swiper/s
 
 const swiper = new Swiper('.swiper-veget', {
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-but-veget-r',
+    prevEl: '.swiper-but-veget-l',
+
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  spaceBetween: 10,
+  loop: false,
+  grabCursor: true,
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        //slidesPerGroup: 1,
+        watchOverflow: false, 
+
+      },
+      760: {
+        slidesPerView: 2,
+        //slidesPerGroup: 1,
+        watchOverflow: false, 
+      }, 
+      1024: {
+        slidesPerView: 3,
+      },
+      1440: {
+        //loopedSlides: 9,
+        slidesPerView: 4,
+        watchOverflow: false,
+
+        //slidesPerGroup: 1,
+        //slidesPerColumn: 2,
+        autoHeight: false,
+      }
+    },
+  
+    
+ 
+}) 
+const swiperFruit = new Swiper('.swiper-fruit', {
+  navigation: {
+    nextEl: '.swiper-but-fruit-r', 
+    prevEl: '.swiper-but-fruit-l',
 
   },
   pagination: {
